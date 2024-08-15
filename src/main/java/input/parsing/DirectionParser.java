06a9aa1fb478f962ad925b78d8a3929e7c6aba27
AbiPetheram <abi.gill97@gmail.com>
2024-08-15 11:05:47 +0100

    feat: initial implementation of parseDirection

--- src/main/java/input/parsing/DirectionParser.java
+++ src/main/java/input/parsing/DirectionParser.java
@@ -1,4 +1,10 @@
 package input.parsing;
 
+import input.CompassDirection;
+
 public class DirectionParser {
+
+    public CompassDirection parseDirection(String[] input){
+        return null;
+    }
 }
