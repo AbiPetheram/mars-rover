b475c92037a9231aa43bbfe26b658ee213a10df6
AbiPetheram <abi.gill97@gmail.com>
2024-08-15 17:36:58 +0100

    feat: implement switch case to update position based on rotate instruction

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -1,3 +1,4 @@
+import input.CompassDirection;
 import input.Instruction;
 import input.Position;
 
@@ -19,6 +20,20 @@ public class Rover {
     }
 
     public void rotate(Instruction instruction){
-
+        if(instruction == Instruction.R){
+            switch(position.getFacing()){
+                case NORTH -> position = new Position(position.getCoordinates(), CompassDirection.EAST);
+                case EAST -> position = new Position(position.getCoordinates(), CompassDirection.SOUTH);
+                case SOUTH -> position = new Position(position.getCoordinates(), CompassDirection.WEST);
+                case WEST -> position = new Position(position.getCoordinates(), CompassDirection.NORTH);
+            }
+        } else if(instruction == Instruction.L){
+            switch(position.getFacing()){
+                case NORTH -> position = new Position(position.getCoordinates(), CompassDirection.WEST);
+                case EAST -> position = new Position(position.getCoordinates(), CompassDirection.NORTH);
+                case SOUTH -> position = new Position(position.getCoordinates(), CompassDirection.EAST);
+                case WEST -> position = new Position(position.getCoordinates(), CompassDirection.SOUTH);
+            }
+        }
     }
 }
