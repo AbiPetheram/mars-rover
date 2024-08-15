7ae14532e061157c53b379e9b0d2c41dd6158641
AbiPetheram <abi.gill97@gmail.com>
2024-08-15 11:41:33 +0100

    feat: initial implementation of parseCoordinates

--- src/main/java/input/parsing/CoordinateParser.java
+++ src/main/java/input/parsing/CoordinateParser.java
@@ -1,4 +1,9 @@
 package input.parsing;
 
+import java.util.HashMap;
+
 public class CoordinateParser {
+    public HashMap<Character, Integer> parseCoordinates(char[] input) {
+        return null;
+    }
 }
