7d61388c827555f79caeeffeeacba62a0b2c9bb2
AbiPetheram <abi.gill97@gmail.com>
2024-08-15 11:27:31 +0100

    feat: implement switch case to return enum based on chracter passed in

--- src/main/java/input/parsing/DirectionParser.java
+++ src/main/java/input/parsing/DirectionParser.java
@@ -5,9 +5,15 @@ import input.CompassDirection;
 public class DirectionParser {
 
     public CompassDirection parseDirection(char input){
-            if(!Character.isAlphabetic(input)){
-                return null;
-            }
-        return CompassDirection.NORTH;
+        if(!Character.isAlphabetic(input)){
+            return null;
+        }
+        return switch (input) {
+            case 'N' -> CompassDirection.NORTH;
+            case 'E' -> CompassDirection.EAST;
+            case 'S' -> CompassDirection.SOUTH;
+            case 'W' -> CompassDirection.WEST;
+            default -> null;
+        };
     }
 }
