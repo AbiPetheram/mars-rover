eea28b2c1dce47f5070664ae1543a6d72a80e859
AbiPetheram <abi.gill97@gmail.com>
2024-08-15 15:30:59 +0100

    test: test string input with all valid instructions

--- src/test/java/input/parsing/InstructionParserTest.java
+++ src/test/java/input/parsing/InstructionParserTest.java
@@ -49,12 +49,21 @@ class InstructionParserTest {
     @Test
     void testLReturnsArrayWithLRMIndividually(){
         InstructionParser ip = new InstructionParser();
-        Instruction[] result = ip.parseInstructions("L");
         assertAll(
                 () -> assertArrayEquals(new Instruction[]{Instruction.L}, ip.parseInstructions("L")),
                 () -> assertArrayEquals(new Instruction[]{Instruction.R}, ip.parseInstructions("R")),
                 () -> assertArrayEquals(new Instruction[]{Instruction.M}, ip.parseInstructions("M"))
                 );
+    }
+
+    @Test
+    void testLReturnsArrayWithMixedStringValidInput(){
+        InstructionParser ip = new InstructionParser();
+        assertAll(
+                () -> assertArrayEquals(new Instruction[]{Instruction.L, Instruction.R, Instruction.M}, ip.parseInstructions("LRM")),
+                () -> assertArrayEquals(new Instruction[]{Instruction.R, Instruction.R, Instruction.R, Instruction.M}, ip.parseInstructions("RRRM")),
+                () -> assertArrayEquals(new Instruction[]{Instruction.M, Instruction.R, Instruction.L, Instruction.M}, ip.parseInstructions("MRLM"))
+        );
     }
 
 }
