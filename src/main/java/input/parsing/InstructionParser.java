cb131c24d51cad75783332dabe78cb3c1f76f177
AbiPetheram <abi.gill97@gmail.com>
2024-08-15 15:28:07 +0100

    feat: implement check which instruction in string for parseInstruction

--- src/main/java/input/parsing/InstructionParser.java
+++ src/main/java/input/parsing/InstructionParser.java
@@ -9,7 +9,11 @@ public class InstructionParser {
         }
         Instruction[] result = new Instruction[input.length()];
         for (int i = 0; i < input.length(); i++){
-            result[i] = Instruction.L;
+            switch(input.toUpperCase().charAt(i)){
+                case 'L' -> result[i] = Instruction.L;
+                case 'R' -> result[i] = Instruction.R;
+                case 'M' -> result[i] = Instruction.M;
+            }
         }
         return result;
     }
