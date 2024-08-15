34ab03198bdef41dcc3470ae12d9627143058c84
AbiPetheram <abi.gill97@gmail.com>
2024-08-15 17:11:49 +0100

    feat: add constructor on position

--- src/main/java/input/Position.java
+++ src/main/java/input/Position.java
@@ -3,4 +3,9 @@ package input;
 public class Position {
     private Coordinates coordinates;
     private CompassDirection facing;
+
+    public Position(Coordinates coordinates, CompassDirection facing) {
+        this.coordinates = coordinates;
+        this.facing = facing;
+    }
 }
