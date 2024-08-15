86e5e9a9d01ec035444f9bb3d5c51bbb2bd232d7
AbiPetheram <abi.gill97@gmail.com>
2024-08-15 17:24:44 +0100

    feat: add getter for position

--- src/main/java/input/Position.java
+++ src/main/java/input/Position.java
@@ -4,6 +4,14 @@ public class Position {
     private Coordinates coordinates;
     private CompassDirection facing;
 
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public CompassDirection getFacing() {
+        return facing;
+    }
+
     public Position(Coordinates coordinates, CompassDirection facing) {
         this.coordinates = coordinates;
         this.facing = facing;
