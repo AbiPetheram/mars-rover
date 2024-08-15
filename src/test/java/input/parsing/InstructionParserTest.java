d79cb779538511bfb708c9aa0d03067a504dd039
AbiPetheram <abi.gill97@gmail.com>
2024-08-15 15:07:21 +0100

    test: add test for null instructions in parseInstructions

--- src/test/java/input/parsing/InstructionParserTest.java
+++ src/test/java/input/parsing/InstructionParserTest.java
@@ -1,7 +1,19 @@
 package input.parsing;
 
+import input.Instruction;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InstructionParserTest {
+
+    @Test
+    void testNullInstructions(){
+        InstructionParser ip = new InstructionParser();
+        Instruction[] result = ip.parseInstructions(null);
+        assertThrows(IllegalArgumentException.class,
+                ()-> ip.parseInstructions(null)
+        );
+    }
 
 }
