338f3f208d83bd8875b6bfda75096e35e3967189
AbiPetheram <abi.gill97@gmail.com>
2024-08-15 17:11:39 +0100

    feat: add constructor on rover

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -1,6 +1,15 @@
 import input.Instruction;
+import input.Position;
 
 public class Rover {
+    private Position position;
+    private Plateau plateau;
+
+    public Rover(Position position, Plateau plateau) {
+        this.position = position;
+        this.plateau = plateau;
+    }
+
     public void rotate(Instruction instruction){
 
     }
