66d0b3b898d162e8f33998a137cb229d03f2be9f
AbiPetheram <abi.gill97@gmail.com>
2024-08-15 15:19:46 +0100

    feat: implement parseInstructions to return instruction L when passed L

--- src/main/java/input/parsing/InstructionParser.java
+++ src/main/java/input/parsing/InstructionParser.java
@@ -4,6 +4,11 @@ import input.Instruction;
 
 public class InstructionParser {
     public Instruction[] parseInstructions(String input){
-        throw new IllegalArgumentException();
+        if(input == null || input.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        Instruction[] result = new Instruction[input.length()];
+        result[0] = Instruction.L;
+        return result;
     }
 }
