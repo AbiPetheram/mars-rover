4e01b052ff2ddea99ba336996a43805c07a6351e
AbiPetheram <abi.gill97@gmail.com>
2024-08-15 17:20:16 +0100

    test: add tests for input rotate right

---
+++ src/test/java/RoverTest.java
@@ -0,0 +1,58 @@
+import input.CompassDirection;
+import input.Coordinates;
+import input.Instruction;
+import input.Position;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoverTest {
+    @Test
+    void testNullInputRotate(){
+        Position position = new Position(new Coordinates(0,0), CompassDirection.EAST);
+        Plateau plateau = new Plateau();
+        Rover rover = new Rover(position, plateau);
+        rover.rotate(null);
+        assertEquals(position, rover.getPosition());
+    }
+
+    @Test
+    void testRotateRightStartingEast(){
+        Position position = new Position(new Coordinates(0,0), CompassDirection.EAST);
+        Plateau plateau = new Plateau();
+        Rover rover = new Rover(position, plateau);
+        rover.rotate(Instruction.R);
+        Position expected = new Position(new Coordinates(0,0), CompassDirection.SOUTH);
+        assertEquals(expected, rover.getPosition());
+    }
+
+    @Test
+    void testRotateRightStartingSouth(){
+        Position position = new Position(new Coordinates(0,0), CompassDirection.SOUTH);
+        Plateau plateau = new Plateau();
+        Rover rover = new Rover(position, plateau);
+        rover.rotate(Instruction.R);
+        Position expected = new Position(new Coordinates(0,0), CompassDirection.WEST);
+        assertEquals(expected, rover.getPosition());
+    }
+
+    @Test
+    void testRotateRightStartingWest(){
+        Position position = new Position(new Coordinates(0,0), CompassDirection.WEST);
+        Plateau plateau = new Plateau();
+        Rover rover = new Rover(position, plateau);
+        rover.rotate(Instruction.R);
+        Position expected = new Position(new Coordinates(0,0), CompassDirection.NORTH);
+        assertEquals(expected, rover.getPosition());
+    }
+
+    @Test
+    void testRotateRightStartingNorth(){
+        Position position = new Position(new Coordinates(0,0), CompassDirection.NORTH);
+        Plateau plateau = new Plateau();
+        Rover rover = new Rover(position, plateau);
+        rover.rotate(Instruction.R);
+        Position expected = new Position(new Coordinates(0,0), CompassDirection.EAST);
+        assertEquals(expected, rover.getPosition());
+    }
+}
