9bbd8c3e8faefa0b71cf8821cdc592e839b4b80b
AbiPetheram <abi.gill97@gmail.com>
2024-08-15 17:21:58 +0100

    test: add tests for input rotate right

--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -55,4 +55,44 @@ class RoverTest {
         Position expected = new Position(new Coordinates(0,0), CompassDirection.EAST);
         assertEquals(expected, rover.getPosition());
     }
+
+    @Test
+    void testRotateLeftStartingEast(){
+        Position position = new Position(new Coordinates(0,0), CompassDirection.EAST);
+        Plateau plateau = new Plateau();
+        Rover rover = new Rover(position, plateau);
+        rover.rotate(Instruction.L);
+        Position expected = new Position(new Coordinates(0,0), CompassDirection.NORTH);
+        assertEquals(expected, rover.getPosition());
+    }
+
+    @Test
+    void testRotateLeftStartingSouth(){
+        Position position = new Position(new Coordinates(0,0), CompassDirection.SOUTH);
+        Plateau plateau = new Plateau();
+        Rover rover = new Rover(position, plateau);
+        rover.rotate(Instruction.L);
+        Position expected = new Position(new Coordinates(0,0), CompassDirection.EAST);
+        assertEquals(expected, rover.getPosition());
+    }
+
+    @Test
+    void testRotateLeftStartingWest(){
+        Position position = new Position(new Coordinates(0,0), CompassDirection.WEST);
+        Plateau plateau = new Plateau();
+        Rover rover = new Rover(position, plateau);
+        rover.rotate(Instruction.L);
+        Position expected = new Position(new Coordinates(0,0), CompassDirection.SOUTH);
+        assertEquals(expected, rover.getPosition());
+    }
+
+    @Test
+    void testRotateLeftStartingNorth(){
+        Position position = new Position(new Coordinates(0,0), CompassDirection.NORTH);
+        Plateau plateau = new Plateau();
+        Rover rover = new Rover(position, plateau);
+        rover.rotate(Instruction.L);
+        Position expected = new Position(new Coordinates(0,0), CompassDirection.WEST);
+        assertEquals(expected, rover.getPosition());
+    }
 }
