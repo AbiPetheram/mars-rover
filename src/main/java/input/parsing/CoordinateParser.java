14c6953d978d4d250f49d383bef19822aa168c60
AbiPetheram <abi.gill97@gmail.com>
2024-08-15 12:26:26 +0100

    feat: implement hashmap returned when valid input passed in

--- src/main/java/input/parsing/CoordinateParser.java
+++ src/main/java/input/parsing/CoordinateParser.java
@@ -4,6 +4,15 @@ import java.util.HashMap;
 
 public class CoordinateParser {
     public HashMap<Character, Integer> parseCoordinates(char[] input) {
-        throw new IllegalArgumentException();
+        HashMap<Character, Integer> result = new HashMap<>();
+        if(input == null || input.length != 2){
+            throw new IllegalArgumentException();
+        } else if (!Character.isDigit(input[0]) || !Character.isDigit(input[1])){
+            throw new IllegalArgumentException();
+        } else {
+            result.put('x', Character.getNumericValue(input[0]));
+            result.put('y', Character.getNumericValue(input[1]));
+        }
+        return result;
     }
 }
