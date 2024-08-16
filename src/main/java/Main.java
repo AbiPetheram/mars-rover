e6dee1f8ab5f3ec55cc9e5e434fa97788da4864f
AbiPetheram <abi.gill97@gmail.com>
2024-08-16 15:24:48 +0100

    feat: create entry point to mars rover

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import input.Position;
+import logic.MissionControl;
+import logic.Plateau;
+import logic.Rover;
+import ui.ConsoleInteraction;
+
+public class Main {
+    public static void main(String[] args) {
+        ConsoleInteraction cs = new ConsoleInteraction();
+        MissionControl mc = new MissionControl();
+        Plateau plateau = cs.getPlateau(mc);
+        Rover rover = cs.addRoverToPlateau(mc, plateau);
+        Position position = cs.moveRover(rover);
+        cs.optionsList(mc, plateau, rover);
+    }
+}
