4bb5e6084750a38546edfbf9784dac83483f1f49
AbiPetheram <abi.gill97@gmail.com>
2024-08-16 11:26:16 +0100

    feat: add createRover and createPlateau methods

--- src/main/java/logic/MissionControl.java
+++ src/main/java/logic/MissionControl.java
@@ -1,4 +1,26 @@
 package logic;
+import input.Coordinates;
+import input.Position;
+
+import java.util.ArrayList;
 
 public class MissionControl {
+    ArrayList<Rover> rovers;
+
+    public MissionControl() {
+        this.rovers = new ArrayList<>();
+    }
+
+    public Plateau createPlateau(Coordinates coordinates){
+        if(coordinates == null){
+            throw new IllegalArgumentException();
+        }
+        return new Plateau(coordinates);
+    }
+
+    public Rover createRover(Plateau plateau, Position position){
+        Rover rover = new Rover(position, plateau);
+        rovers.add(rover);
+        return rover;
+    }
 }
