3a172ba8494f0cdcf411f43a4db65f52704f46f6
AbiPetheram <abi.gill97@gmail.com>
2024-08-16 11:25:47 +0100

    test: test valid input for create plateau

--- src/test/java/logic/MissionControlTest.java
+++ src/test/java/logic/MissionControlTest.java
@@ -1,7 +1,10 @@
 package logic;
 
+import input.Coordinates;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MissionControlTest {
@@ -12,6 +15,13 @@ class MissionControlTest {
         assertThrows(IllegalArgumentException.class,
                 ()-> mc.createPlateau(null)
         );
+    }
+
+    @Test
+    void createPlateauValidInput(){
+        MissionControl mc = new MissionControl();
+        Plateau expected = new Plateau(new Coordinates(2,3));
+        assertThat(mc.createPlateau(new Coordinates(2,3)), samePropertyValuesAs(expected));
     }
 
 }
