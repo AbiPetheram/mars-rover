7a83e33db5148a832e2b79d4d9d9fd7b6dbad1ec
AbiPetheram <abi.gill97@gmail.com>
2024-08-16 10:36:30 +0100

    feat: update plateau to use coordinates as size

--- src/main/java/Plateau.java
+++ src/main/java/Plateau.java
@@ -1,2 +1,14 @@
+import input.Coordinates;
+
 public class Plateau {
+    private Coordinates size;
+
+    public Plateau(Coordinates size) {
+        this.size = size;
+    }
+
+    public Coordinates getSize() {
+        return size;
+    }
+
 }
