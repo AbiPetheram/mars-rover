1fd6fe71041b982e8cae92a981ae3a6d6ee78070
AbiPetheram <abi.gill97@gmail.com>
2024-08-16 11:22:18 +0100

    test: test null input to create new plateau

--- src/test/java/logic/MissionControlTest.java
+++ src/test/java/logic/MissionControlTest.java
@@ -1,7 +1,17 @@
 package logic;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MissionControlTest {
+
+    @Test
+    void createPlateauNullInput(){
+        MissionControl mc = new MissionControl();
+        assertThrows(IllegalArgumentException.class,
+                ()-> mc.createPlateau(null)
+        );
+    }
 
 }
